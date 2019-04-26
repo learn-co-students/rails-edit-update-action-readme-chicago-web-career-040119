67004a7f4ff4a4912d59cb5483d8fc6e0e5b1024
NMoore42 <nicholas.moore5@icloud.com>
2019-04-26 16:04:17 -0500

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,7 +26,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    
     @article = Article.find(params[:id])
     @article.update(title: params[:article][:title], description: params[:article][:description])
     redirect_to article_path(@article)
